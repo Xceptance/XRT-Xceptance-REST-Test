7a9d8cca532e5536099863baea367fadfc5d99bd
Patrick Thaele <p.thaele@xceptance.net>
2016-03-23 16:21:00 +0100

    Fixing defect #1: Adding basic authentication support in URL.

---
+++ src/main/java/com/xceptance/xrt/annotation/authentication/BasicAuth.java
@@ -0,0 +1,32 @@
+package com.xceptance.xrt.annotation.authentication;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * A class provided in this annotation is instantiated by XRT. With <b>reuseInstance</b> this instance is stored for the
+ * whole test case. If <b>reuseInstance</b> is false, XRT instantiates a new object with every REST call.
+ *
+ * @author Patrick Thaele
+ */
+@Retention( RetentionPolicy.RUNTIME )
+@Target( { ElementType.TYPE } )
+public @interface BasicAuth
+{
+    /**
+     * The class that is used to perform the pre-processing.
+     *
+     * @return The class that is used to perform the pre-processing.
+     */
+    String username();
+
+    /**
+     * With <b>reuseInstance</b> this instance is stored for the
+     * whole test case. If <b>reuseInstance</b> is false, XRT instantiates a new object with every REST call.
+     *
+     * @return <b>true</b> if the instance used for pre-processing is used in the whole test, <b>false</b> if not.
+     */
+    String password() default "";
+}
